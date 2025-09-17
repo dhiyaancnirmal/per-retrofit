2ce649e3a44f7a1ee37b88a18290ed8b53d38e93
shb-png <imshriyabalu@gmail.com>
2025-09-17 12:45:01 -0400

    Added basic Magnetometer sensor setup

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 // Pins for CAN1: (CRX1 -> pin 23) (CTX1 --> pin 22)
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1; // Use CAN 2.0 constructor template
 
+
+// Pins for I2C: (SCL --> pin 19) (SDA --> pin 18)
+Wire.begin()
+DFRobot_BMM350_I2C bmm350(&Wire, I2C_ADDRESS);
+
 // put function declarations here:
 void canSniff(const CAN_message_t &msg);
 
@@ -19,7 +24,17 @@ void setup() {
   can1.mailboxStatus();
 
   Serial.begin(115200);
-  
+  while(!Serial);
+  while(bmm350.begin()){
+    Serial.println("bmm350 init failed, Please try again!");
+    delay(1000);
+  } Serial.println("bmm350 init success!");
+
+  Serial.println(bmm350.selfTest());
+  bmm350.setOperationMode(BMM350_NORMAL_MODE);
+  bmm350.setPresetMode(BMM350_PRESETMODE_HIGHACCURACY);
+  bmm350.setRate(BMM350_DATA_RATE_25HZ);
+  bmm350.setMeasurementXYZ();
 }
 
 
@@ -33,6 +48,19 @@ void loop() {
     CAN_message_t msg;
     timeout = millis();
   }
+
+  // Geomagnetic data from BMM350 Sensor
+  // TODO: save sensor information and send through CAN bus instead
+  sBmm350MagData_t magData = bmm350.getGeomagneticData();
+  Serial.print("mag x = "); Serial.print(magData.x); Serial.println(" uT");
+  Serial.print("mag y = "); Serial.print(magData.y); Serial.println(" uT");
+  Serial.print("mag z = "); Serial.print(magData.z); Serial.println(" uT");
+
+  float compassDegree = bmm350.getCompassDegree();
+  Serial.print("the angle between the pointing direction and north (counterclockwise) is:");
+  Serial.println(compassDegree);
+  Serial.println("--------------------------------");
+  delay(3000);
 }
 
 // put function definitions here:
